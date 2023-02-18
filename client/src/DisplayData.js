530b957d4e98157959e58dea31143f423547f17a
nishitchittora <nishitchittora2015@gmail.com>
2023-02-18 23:31:13 +0530

    useMutation added

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,4 +1,4 @@
-import { useQuery, gql, useLazyQuery } from "@apollo/client";
+import { useQuery, gql, useLazyQuery, useMutation } from "@apollo/client";
 
 const QUERY_ALL_USER = gql`
 	query GetAllUsers {
@@ -24,14 +24,41 @@ const GET_MOVIE_BY_NAME = gql`
 	}
 `;
 
+const CREATE_USER_MUTATION = gql`
+	mutation CreateUser($user: CreateUserInput!) {
+		createUser(user: $user) {
+			name
+			username
+			age
+		}
+	}
+`;
+
 function DisplayData() {
-	const { data, loading, error } = useQuery(QUERY_ALL_USER);
+	const { data, loading, error, refetch } = useQuery(QUERY_ALL_USER);
 	const [fetchMovie, { data: movieSearchedData, error: movieError }] =
 		useLazyQuery(GET_MOVIE_BY_NAME);
-
+	const [createUser] = useMutation(CREATE_USER_MUTATION);
 	console.log(movieSearchedData);
 	return (
 		<h1>
+			<div
+				onClick={() => {
+					createUser({
+						variables: {
+							user: {
+								name: "nishit1",
+								username: "worldwide1",
+								age: 28,
+								nationality: "BRAZIL",
+							},
+						},
+					});
+					refetch();
+				}}
+			>
+				Click here to create User
+			</div>
 			List User
 			{data && data?.users?.map((user) => <div>{user.name}</div>)}
 			<div
